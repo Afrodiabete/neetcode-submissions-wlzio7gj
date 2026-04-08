b81d04817918d94c18cb00bd83c1612fb2ccb386
Afrodiabete <107562639+Afrodiabete@users.noreply.github.com>
2026-04-08 18:52:23 -0400

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* curr= nullptr, *ans= nullptr;
+
+        
+        while(list1!= nullptr || list2!= nullptr){
+            if(curr== nullptr){
+                if(list1== nullptr){
+                    curr= list2;
+                    list2= list2->next;
+                }
+                else if(list2== nullptr){
+                    curr= list1;
+                    list1= list1->next;
+                }
+                else{
+                    if(list1->val> list2->val){
+                        curr= list2; list2= list2->next;
+                    }
+                    else{
+                        curr= list1; list1= list1->next;
+                    }
+                }
+                ans= curr;
+            }
+            else{
+                if(list1== nullptr){
+                    curr->next= list2;
+                    curr= curr->next;
+                    list2= list2->next;
+                }
+                else if(list2== nullptr){
+                    curr->next= list1;
+                    curr= curr->next;
+                    list1= list1->next;
+                }
+                else{
+                    if(list1->val> list2->val){
+                        curr->next= list2; list2= list2->next;
+                    }
+                    else{
+                        curr->next= list1; list1= list1->next;
+                    }
+                    curr= curr->next;
+                }
+            }
+        }
+        return ans;
+    }
+};
